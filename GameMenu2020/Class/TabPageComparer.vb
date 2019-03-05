ab5379e2f0edcc846dc759caf3bc732a937cbf6a
Bartholomew Ho <qiangqiang101@hotmail.com>
2019-03-06 02:40:08 +0800

    aaa

--- GameMenu2020/Class/TabPageComparer.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class TabPageComparer
-    Implements IComparer(Of TabPage)
-
-    Public Function Compare(ByVal x As TabPage, ByVal y As TabPage) As Integer Implements IComparer(Of TabPage).Compare
-        Return String.Compare(x.Text, y.Text)
-    End Function
-End Class
